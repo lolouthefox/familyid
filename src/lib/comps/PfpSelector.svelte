e948302cfaa403c005d43ae4ba4d1d03b9f4de98
Liam Cheneval <liam.cheneval@gmail.com>
2026-07-06 16:54:49 +0200

    Profile pictures test1

--- src/lib/comps/PfpSelector.svelte
+++ src/lib/comps/PfpSelector.svelte
@@ -23,12 +23,21 @@
 	};
 </script>
 
-{#each Object.keys(profilePictures) as packName, i (i)}
-	{@const pack = profilePictures[packName]}
-	<div>
-		<h2>{packName}</h2>
-		{#each new Array(pack.number) as _, i (i)}
-			<img src={pack.path + (i + 1) + '.png'} alt={packName} />
+<div class="max-h-dvh overflow-y-scroll">
+	<div class="flex flex-col gap-4">
+		{#each Object.keys(profilePictures) as packName, i (i)}
+			{@const pack = profilePictures[packName]}
+
+			<div class="flex flex-col gap-2">
+				<h2 class="text-xl font-bold">{packName}</h2>
+				<div class="w-full overflow-x-scroll">
+					<div class="flex gap-2">
+						{#each new Array(pack.number) as _, i (i)}
+							<img src={pack.path + (i + 1) + '.png'} alt={packName} class="h-32 rounded-2xl" />
+						{/each}
+					</div>
+				</div>
+			</div>
 		{/each}
 	</div>
-{/each}
+</div>
